971afbaa7f33aa94a10f4d57e218ae047599d663
KavishaHaswani <141186389+KavishaHaswani@users.noreply.github.com>
2026-07-18 18:34:37 +0530

    1400 - fortune telling

---
+++ 1400/fortuneTelling.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, x, y;
+        cin >> n >> x >> y;
+        vector<int> a(n);
+        for (auto &i : a)
+        {
+            cin >> i;
+        }
+
+        for (auto i : a)
+        {
+            x = (x + i) % 2;
+        }
+
+        if (x == y % 2)
+        {
+            cout << "Alice\n";
+        }
+        else
+        {
+            cout << "Bob\n";
+        }
+    }
+}
